0e97b2dc64a2aad8cf6a4dd03b6e0a0121818038
xiong-binbin <57823629+xiong-binbin@users.noreply.github.com>
2020-11-03 22:10:56 +0800

    create basic version

---
+++ web/src/components/Index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="index">
+      Hello
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'index',
+    data () {
+      return {
+      }
+    },
+    methods: {
+    },
+    mounted() {
+    },
+  }
+</script>
+
+<style scoped>
+.index {
+  padding: 50px 100px 0px 100px;
+}
+</style>
