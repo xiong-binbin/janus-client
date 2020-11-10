4ecc797d618a3d6b7299efa32271ebecfbc61487
xiong-binbin <57823629+xiong-binbin@users.noreply.github.com>
2020-11-10 23:28:46 +0800

    update

--- web/src/components/Echo.vue
+++ web/src/components/Echo.vue
@@ -13,6 +13,10 @@
 
     data () {
       return {
+        janus: null,
+        echotest: null,
+        opaqueId: "echotest-"+Janus.randomString(12),
+        host: "https://www.xiongbinbin.club:8089/janus",  // "https://" + window.location.hostname + ":8089/janus",
       }
     },
 
@@ -25,9 +29,68 @@
         dependencies: Janus.useDefaultDependencies({
           adapter: adapter
         }),
-        callback: function() {
-            console.log('----');
-        }});
+        callback: ()=> {
+          if(!Janus.isWebrtcSupported()) {
+            console.log('is not Supported Webrtc!');
+            return;
+          }
+
+          this.janus = new Janus({
+          server: this.host,
+          success: ()=> {
+            this.janus.attach({
+              plugin: "janus.plugin.echotest",
+              opaqueId: this.opaqueId,
+              success: (pluginHandle)=> {
+                console.log('success');
+              },
+              error: (error)=> {
+                console.log('error');
+              },
+              consentDialog: (on)=> {
+                console.log('consentDialog');
+              },
+              iceState: (state)=> {
+                console.log('iceState');
+              },
+              mediaState: (medium, on)=> {
+                console.log('mediaState');
+              },
+              webrtcState: (on)=> {
+                console.log('webrtcState');
+              },
+              slowLink: (uplink, lost)=> {
+                console.log('slowLink');
+              },
+              onmessage: (msg, jsep)=> {
+                console.log('onmessage');
+              },
+              onlocalstream: (stream)=> {
+                console.log('onlocalstream');
+              },
+              onremotestream: (stream)=> {
+                console.log('onremotestream');
+              },
+              ondataopen: (data)=> {
+                console.log('ondataopen');
+              },
+              ondata: (data)=> {
+                console.log('ondata');
+              },
+              oncleanup: ()=> {
+                console.log('oncleanup');
+              }
+            });
+          },
+          error: (error)=> {
+            Janus.error(error);
+            console.log(error);
+          },
+          destroyed: ()=> {
+            window.location.reload();
+          }
+        });
+      }});
     },
   }
 </script>
